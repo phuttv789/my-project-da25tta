fcbbadd62285bc84df814fbc7db76bd0fd0e6af2
phu <giaphunguyen054@gmail.com>
2026-06-10 15:16:19 +0700

    update code

--- js/lib.js
+++ js/lib.js
@@ -1,52 +1,62 @@
- const product ={name: "Giày Bóng Đá NEKI",
-                price:"2.790.000VND/1 đôi",
-                description : "Được sản xuất bởi hãng NIKE.",
-                image:"../assets/giaydabanh.jpg"}
-function inBCC(n)
-{
-    let result="";
-    for( let i=1; i<=10; i++)
+const products = [
+    {name: "Giày Bóng Đá NEKI",
+    price: "2.790.000VND/1 đôi",
+    description: "Được sản xuất bởi hãng NIKE.",
+    image: "../assets/giaydabanh.jpg"},
+
     {
-        result += `${n} x ${i} = ${n*i}<br>`;
+        name: "Giày Bóng Đá NEKI",
+    price: "2.790.000VND/1 đôi",
+    description: "Được sản xuất bởi hãng NIKE.",
+    image: "../assets/giaydabanh.jpg"
     }
- document.getElementById('result').innerHTML= result;
+]
+
+function inBCC(n) {
+    let result = "";
+    for (let i = 1; i <= 10; i++) {
+        result += `${n} x ${i} = ${n * i}<br>`;
+    }
+    document.getElementById('result').innerHTML = result;
 }
 
-function createItem(obj)
-{
+function createItem(obj) {
     const item = document.createElement("div");
-    item.setAttribute("class","container=item")
-    //tao 2 khung chua con 
+    item.setAttribute("class", "container-item");
+
+    const containerImage = document.createElement("div");
+    containerImage.setAttribute("class", "container-image");
+
     const image = document.createElement("img");
-    image.setAttribute("src",obj.image)
-    image.setAttribute("alt",obj.name)
-    containerImage.appendchild(Image);
+    image.setAttribute("src", obj.image);
+    image.setAttribute("alt", obj.name);
+    image.setAttribute("height","400px");
+    image.setAttribute("widgth", "400px");
+    containerImage.appendChild(image);
 
     const containerInfo = document.createElement("div");
-    containerInfo.setAttribute("class","container-Info");
-    //chen 2 khung chua vao container-item
-    item.appendchild(containerImage);
-    item.appendChild(containerInfo);
+    containerInfo.setAttribute("class", "container-Info");
 
-    document.getElementById("product-list").appendChild(item);
+    item.appendChild(containerImage);
+    item.appendChild(containerInfo);
 
     const name = document.createElement("p");
     name.innerHTML = obj.name;
 
-     const price = document.createElement("p");
+    const price = document.createElement("p");
     price.innerHTML = obj.price;
 
     const description = document.createElement("p");
     description.innerHTML = obj.description;
+
     const link = document.createElement("a");
     link.innerHTML = "more";
-    link.setAttribute("href",obj.linkProducct);
+    link.setAttribute("href", obj.linkProduct);
 
     containerInfo.appendChild(name);
     containerInfo.appendChild(price);
     containerInfo.appendChild(description);
     containerInfo.appendChild(link);
-    containerInfo.appendChild(image);
 
     document.getElementById("product-list").appendChild(item);
 }
